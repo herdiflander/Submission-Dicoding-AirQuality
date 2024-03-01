81f9e78317be9c5ad95ac865902dda495635cbed
herdiflander <132785919+herdiflander@users.noreply.github.com>
2024-03-01 22:53:49 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import urllib
 from statsmodels.tsa.seasonal import seasonal_decompose
 import numpy as np
 
